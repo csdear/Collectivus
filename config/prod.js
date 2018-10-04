97b4d25d41737981c88c212599b729d9f8c31356
csdear <stuart.dear@intermarkgroup.com>
2018-10-03 20:13:21 -0500

    fine. ill allow you to to ref the dev database eventhough you are prod.js key file...

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 //dev.js, dont commit this.
 module.exports = {
-    //mongoURI: 'mongodb://csdear:agotcha1@ds121373.mlab.com:21373/collectivus_dev'
+    mongoURI: 'mongodb://csdear:agotcha1@ds121373.mlab.com:21373/collectivus_dev'
 };
